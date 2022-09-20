d4eae441c83b45f1dc6dd5b82b059e263079a2a1
challengedev210 <challengedev210@gmail.com>
2022-09-20 08:28:32 -0400

    e

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -58,56 +58,6 @@ function Home2() {
             </Tilt>
           </Col>
         </Row>
-        <Row>
-          <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
-            <p>
-              Feel free to <span className="purple">connect </span>with me
-            </p>
-            <ul className="home-about-social-links">
-              <li className="social-icons">
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiFillGithub />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <FaTelegram />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <FaSlack />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiFillSkype />
-                </a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
       </Container>
     </Container>
   );
